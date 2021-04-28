c70886a996aad17929ccec5093fe4c1a4536c53c
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2021-04-28 14:18:17 +0200

    Setup database

--- lib/my-service-stack.ts
+++ lib/my-service-stack.ts
@@ -1,15 +1,34 @@
-import { IVpc, Vpc } from "aws-cdk-lib/aws-ec2";
+import { IVpc, Vpc, InstanceClass,
+  InstanceSize,
+  InstanceType, } from "aws-cdk-lib/aws-ec2";
 import * as cdk from 'aws-cdk-lib';
+import {
+  Credentials,
+  DatabaseInstance,
+  DatabaseInstanceEngine, MysqlEngineVersion,
+} from "aws-cdk-lib/aws-rds";
 import { Construct } from "constructs";
-import { stackNameOf } from "./utils";
+import { stackNameOf, ownerSpecificName } from "./utils";
 
 interface MyServiceProps {
   vpc: IVpc
 }
 
 export class MyServiceStack extends cdk.Stack {
+
   constructor(scope: Construct, props: MyServiceProps) {
     super(scope, stackNameOf(MyServiceStack),);
 
+    const instance = new DatabaseInstance(this, 'Database', {
+      vpc: props.vpc,
+      instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.MICRO),
+      engine: DatabaseInstanceEngine.mysql({
+        version: MysqlEngineVersion.VER_8_0
+      }),
+      multiAz: false,
+      instanceIdentifier: ownerSpecificName("my-service"),
+      databaseName: "service",
+      credentials: Credentials.fromGeneratedSecret("service")
+    });
   }
 }

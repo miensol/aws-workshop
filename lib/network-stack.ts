9b61534e47b9068172b06d56a4481ac95bafde18
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2021-04-28 08:25:41 +0200

    Base for deploying

--- lib/network-stack.ts
+++ lib/network-stack.ts
@@ -1,9 +1,15 @@
+import { IVpc, Vpc } from "@aws-cdk/aws-ec2";
 import * as cdk from '@aws-cdk/core';
 import { stackNameOf } from "./utils";
 
 export class NetworkStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, props?: cdk.StackProps) {
-    super(scope, stackNameOf(NetworkStack), );
+  readonly vpc: IVpc;
 
+  constructor(scope: cdk.Construct, props?: cdk.StackProps) {
+    super(scope, stackNameOf(NetworkStack),);
+
+    this.vpc = new Vpc(this, 'VPC', {
+      natGateways: 1
+    })
   }
 }

831e6e91c5c4c803f338a36c77cf0824538c6cf5
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2021-11-15 13:27:39 +0100

    Network setup complete

--- lib/network-stack.ts
+++ lib/network-stack.ts
@@ -1,3 +1,4 @@
+import { Vpc } from "@aws-cdk/aws-ec2";
 import * as cdk from '@aws-cdk/core';
 import { stackNameOf } from "./utils";
 
@@ -5,5 +6,8 @@ export class NetworkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, props?: cdk.StackProps) {
     super(scope, stackNameOf(NetworkStack), );
 
+    new Vpc(this, 'VPC', {
+      natGateways: 1
+    })
   }
 }

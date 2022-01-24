4b8163a70d1728bf997a921fe59f764521b5df7f
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2022-01-24 19:28:14 +0100

    Upgrade to cdk 2.0

--- lib/network-stack.ts
+++ lib/network-stack.ts
@@ -1,13 +1,15 @@
-import { Vpc } from "aws-cdk-lib/aws-ec2";
+import { IVpc, Vpc } from "aws-cdk-lib/aws-ec2";
 import * as cdk from 'aws-cdk-lib';
 import { stackNameOf } from "./utils";
 import { Construct } from "constructs";
 
 export class NetworkStack extends cdk.Stack {
+  readonly vpc: IVpc;
+
   constructor(scope: Construct, props?: cdk.StackProps) {
     super(scope, stackNameOf(NetworkStack), );
 
-    new Vpc(this, 'VPC', {
+    this.vpc = new Vpc(this, 'VPC', {
       natGateways: 1
     })
   }

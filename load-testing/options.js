d2c4e62671fe646504efa83ce987ecca99e81ab8
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2022-02-20 09:52:49 +0100

    Fix day5-base version

--- load-testing/options.js
+++ load-testing/options.js
@@ -1,7 +1,7 @@
 export const options = {
   stages: [
-    { duration: '30s', target: 20 },
-    { duration: '2m', target: 50 },
-    { duration: '2m', target: 100 },
+    { duration: '30s', target: 200 },
+    { duration: '2m', target: 500 },
+    { duration: '2m', target: 1000 },
   ],
 }

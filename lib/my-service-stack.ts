fd91788e231280851606b48950072068a0db2b80
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2022-12-20 15:29:38 +0100

    Fixed aws ecs fargate integration

--- lib/my-service-stack.ts
+++ lib/my-service-stack.ts
@@ -1,19 +1,29 @@
 import * as cdk from 'aws-cdk-lib'
 import { CfnOutput, RemovalPolicy } from 'aws-cdk-lib'
 import { Certificate, CertificateValidation } from 'aws-cdk-lib/aws-certificatemanager'
-import { BastionHostLinux, InstanceClass, InstanceSize, InstanceType, IVpc, Port, } from 'aws-cdk-lib/aws-ec2'
-import { Cluster, ContainerImage, FargateService, FargateTaskDefinition, Secret } from 'aws-cdk-lib/aws-ecs'
+import {
+  BastionHostLinux,
+  InstanceClass,
+  InstanceSize,
+  InstanceType,
+  IVpc,
+  Port,
+  SecurityGroup,
+} from 'aws-cdk-lib/aws-ec2'
+import { Cluster, ContainerImage, FargateService, FargateTaskDefinition, LogDriver, Secret } from 'aws-cdk-lib/aws-ecs'
 import { Credentials, DatabaseInstance, DatabaseInstanceEngine, MysqlEngineVersion, } from 'aws-cdk-lib/aws-rds'
 import { IPublicHostedZone } from 'aws-cdk-lib/aws-route53'
 import { Construct } from 'constructs'
 import { ownerName, ownerSpecificName, stackNameOf } from './utils'
-import { HttpApi, HttpMethod, IVpcLink } from '@aws-cdk/aws-apigatewayv2-alpha'
+import { HttpApi, HttpMethod, IVpcLink, VpcLink } from '@aws-cdk/aws-apigatewayv2-alpha'
 import { HttpLambdaIntegration, HttpServiceDiscoveryIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha'
 import { IPrivateDnsNamespace } from 'aws-cdk-lib/aws-servicediscovery/lib/private-dns-namespace'
 import { CfnApiGatewayManagedOverrides, CfnStage } from 'aws-cdk-lib/aws-apigatewayv2'
 import { LogGroup } from 'aws-cdk-lib/aws-logs'
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
 import * as path from 'path'
+import { ApplicationLoadBalancer, ApplicationProtocol } from 'aws-cdk-lib/aws-elasticloadbalancingv2'
+import { DnsRecordType } from 'aws-cdk-lib/aws-servicediscovery'
 import AccessLogSettingsProperty = CfnApiGatewayManagedOverrides.AccessLogSettingsProperty
 
 interface MyServiceProps {
@@ -51,6 +61,10 @@ export class MyServiceStack extends cdk.Stack {
     }
 
     const taskDefinition = new FargateTaskDefinition(this, 'PhpMyAdminTask', {})
+    const phpmyadminLogGroup = new LogGroup(this, 'phpmyadmin log group', {
+      logGroupName: ownerSpecificName('phpmyadmin'),
+      removalPolicy: RemovalPolicy.DESTROY
+    })
 
     taskDefinition.addContainer('phpmyadmin', {
       image: ContainerImage.fromRegistry('phpmyadmin/phpmyadmin'),
@@ -63,9 +77,14 @@ export class MyServiceStack extends cdk.Stack {
       secrets: {
         PMA_USER: Secret.fromSecretsManager(databaseInstance.secret!, 'username'),
         PMA_PASSWORD: Secret.fromSecretsManager(databaseInstance.secret!, 'password'),
-      }
+      },
+      logging: LogDriver.awsLogs({
+        logGroup: phpmyadminLogGroup,
+        streamPrefix: 'phpmyadmin'
+      })
     })
-    const fargateService = new FargateService(this, 'PhpMyAdmin', {
+
+    const fargateService = new FargateService(this, 'php-my-admin', {
       cluster: new Cluster(this, 'Cluster', {
         vpc: props.vpc,
       }),
@@ -73,11 +92,11 @@ export class MyServiceStack extends cdk.Stack {
       cloudMapOptions: {
         cloudMapNamespace: props.privateNamespace,
         name: 'phpmyadmin',
-        containerPort: 80
+        containerPort: 80,
+        // https://docs.aws.amazon.com/apigateway/latest/developerguide/http-api-develop-integrations-private.html#http-api-develop-integrations-private-Cloud-Map
+        dnsRecordType: DnsRecordType.SRV // only one supported by API gateway
       }
     })
-
-    fargateService.connections.allowFromAnyIpv4(Port.tcp(80), 'Allow connecting from VPC')
 
     databaseInstance.connections.allowDefaultPortFrom(fargateService.connections, 'PhpMyAdmin')
 
@@ -91,9 +110,20 @@ export class MyServiceStack extends cdk.Stack {
       validation: CertificateValidation.fromDns(brightDevZone)
     })
 
+    const securityGroup = new SecurityGroup(this, 'vpc link security group', {
+      vpc: props.vpc,
+    })
+
+    securityGroup.connections.allowFromAnyIpv4(Port.tcp(80), 'Allow gateway to reach on port 80')
+
+    fargateService.connections.allowFrom(securityGroup.connections, Port.tcp(80), 'Allow connecting from gateway vpc link')
+
     const httpApi = new HttpApi(this, 'api gateway', {
       defaultIntegration: new HttpServiceDiscoveryIntegration('phpmyadmin-default', fargateService.cloudMapService!, {
-        vpcLink: props.gatewayVpcLink,
+        vpcLink: new VpcLink(this, 'vpc link', {
+          vpc: props.vpc,
+          securityGroups: [securityGroup]
+        }),
       }),
     })
 
@@ -105,20 +135,12 @@ export class MyServiceStack extends cdk.Stack {
     const defaultStage = httpApi.defaultStage!.node.defaultChild as CfnStage
     defaultStage.accessLogSettings = {
       destinationArn: apiGatewayLogGroup.logGroupArn,
-      format: '$context.identity.sourceIp - - [$context.requestTime] "$context.httpMethod $context.routeKey $context.protocol" $context.status $context.responseLength $context.requestId $context.error.message'
+      format: 'requestId=$context.requestId integrationErrorMessage = $context.integrationErrorMessage error.message = $context.error.message'
     } as AccessLogSettingsProperty
 
     httpApi.addRoutes({
-      path: '/',
-      methods: [ HttpMethod.ANY ],
-      integration: new HttpServiceDiscoveryIntegration('phpmyadmin', fargateService.cloudMapService!, {
-        vpcLink: props.gatewayVpcLink,
-      }),
-    })
-
-    httpApi.addRoutes({
       path: '/lambda',
-      methods: [ HttpMethod.ANY ],
+      methods: [HttpMethod.ANY],
       integration: new HttpLambdaIntegration('whoami', new NodejsFunction(this, 'whoami', {
         vpc: props.vpc,
         entry: path.join(process.cwd(), 'lib', 'whoami.lambda.ts'),
@@ -130,6 +152,9 @@ export class MyServiceStack extends cdk.Stack {
 
     new CfnOutput(this, 'phpmyadmin FQDN', {
       value: phpMyAdminFQDN
+    })
+    new CfnOutput(this, 'phpmyadmin api gateway', {
+      value: httpApi.apiEndpoint
     })
   }
 }
--- lib/whoami.lambda.ts
+++ lib/whoami.lambda.ts
@@ -2,5 +2,5 @@ import { APIGatewayProxyHandlerV2 } from 'aws-lambda'
 
 export const handler: APIGatewayProxyHandlerV2 = async (event) => {
   console.log({ event })
-  return { statusCode: 200, body: JSON.stringify({ owner: process.env.OWNER_NAME }) }
+  return { statusCode: 200, body: JSON.stringify({ owner: process.env.OWNER_NAME, body: event.body }) }
 }

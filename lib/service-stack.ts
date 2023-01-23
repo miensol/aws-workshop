23750f97f35557d2fb5a84944164219a0028b223
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2023-01-23 14:10:07 +0100

    EKS IAm

--- lib/service-stack.ts
+++ lib/service-stack.ts
@@ -1,7 +1,8 @@
 import { Stack } from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 import { stackNameOf } from './utils'
-import { HelmChart, ICluster } from 'aws-cdk-lib/aws-eks'
+import { HelmChart, ICluster, KubernetesManifest, ServiceAccount } from 'aws-cdk-lib/aws-eks'
+import { Bucket } from 'aws-cdk-lib/aws-s3'
 
 interface ServiceStackProps {
   cluster: ICluster
@@ -17,11 +18,46 @@ export class ServiceStack extends Stack {
     //   repository: 'https://charts.bitnami.com/bitnami'
     // })
 
+    const namespace = 'service'
     new HelmChart(this, 'postgres', {
       cluster: props.cluster,
-      namespace: 'service',
+      namespace: namespace,
       chart: 'postgresql',
       repository: 'https://charts.bitnami.com/bitnami',
     })
+
+    const serviceAccount = new ServiceAccount(this, 'mypod account', {
+      cluster: props.cluster,
+      name: 'mypod',
+      namespace: namespace
+    })
+
+    const bucket = new Bucket(this, 'Bucket')
+    bucket.grantReadWrite(serviceAccount)
+
+    const mypod = new KubernetesManifest(this, 'mypod', {
+      cluster: props.cluster,
+      manifest: [{
+        apiVersion: 'v1',
+        kind: 'Pod',
+        metadata: {
+          name: 'mypod',
+          namespace: namespace
+        },
+        spec: {
+          serviceAccountName: serviceAccount.serviceAccountName,
+          containers: [
+            {
+              name: 'hello',
+              image: 'paulbouwer/hello-kubernetes:1.5',
+              ports: [{ containerPort: 8080 }],
+            },
+          ],
+        },
+      }]
+    })
+
+// create the resource after the service account.
+    mypod.node.addDependency(serviceAccount)
   }
 }

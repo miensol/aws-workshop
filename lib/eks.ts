afad4f54a7c5dc884a8c20c8c1128d50cbf16e61
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2023-01-23 15:03:53 +0100

    Add eks admin

--- lib/eks.ts
+++ lib/eks.ts
@@ -3,6 +3,9 @@ import { Construct } from 'constructs'
 import { ownerSpecificName, stackNameOf } from './utils'
 import { InstanceClass, InstanceSize, InstanceType, IVpc } from 'aws-cdk-lib/aws-ec2'
 import { CapacityType, Cluster, KubernetesVersion } from 'aws-cdk-lib/aws-eks'
+import { CfnOutput } from 'aws-cdk-lib'
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam'
+import { Arn } from 'aws-cdk-lib'
 
 interface EksClusterProps {
   vpc: IVpc
@@ -14,14 +17,29 @@ export class EKS extends cdk.Stack {
   constructor (scope: Construct, props: EksClusterProps) {
     super(scope, stackNameOf(EKS))
 
+    const clusterName = ownerSpecificName('cluster')
     this.cluster = new Cluster(this, 'cluster', {
       vpc: props.vpc, version: KubernetesVersion.V1_24,
-      clusterName: ownerSpecificName('cluster'),
+      clusterName: clusterName,
     })
+
+    // make it possible to use from web us
+    this.cluster.adminRole.addToPolicy(new PolicyStatement({
+      actions: ['eks:*'],
+      resources: [Arn.format({
+        resource: 'cluster',
+        resourceName: clusterName,
+        service: 'eks'
+      }, this)]
+    }))
 
     this.cluster.addNodegroupCapacity('spot-t3', {
       capacityType: CapacityType.SPOT,
       instanceTypes: [InstanceType.of(InstanceClass.T3, InstanceSize.MEDIUM), InstanceType.of(InstanceClass.T3, InstanceSize.SMALL)]
+    })
+
+    new CfnOutput(this, 'AdminRole', {
+      value: this.cluster.adminRole.roleArn
     })
   }
 }
